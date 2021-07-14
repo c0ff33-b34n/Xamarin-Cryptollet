77ab1e189e7e51510979c71911af119a812e9c56
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-14 14:12:17 +0100

    Delete unneccesary template files, organise folder structure and add android image assets

--- Cryptollet/Application/App.xaml.cs
+++ Cryptollet/Application/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Cryptollet.Services;
-using Cryptollet.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace Cryptollet
 {
@@ -12,8 +8,6 @@ namespace Cryptollet
         public App()
         {
             InitializeComponent();
-
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
