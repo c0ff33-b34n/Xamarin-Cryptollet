b59b672a3bd96ebbfe145fe02db9384188cc9447
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-16 14:25:25 +0100

    Implement Add Transactions View

--- Cryptollet/Modules/AddTransaction/AddTransactionViewModel.cs
+++ Cryptollet/Modules/AddTransaction/AddTransactionViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Cryptollet.Common.Base;
+using Cryptollet.Common.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Cryptollet.Modules.AddTransaction
@@ -9,13 +13,17 @@ namespace Cryptollet.Modules.AddTransaction
     [QueryProperty("Id", "id")]
     public class AddTransactionViewModel : BaseViewModel
     {
-        private bool _isDeposit;
+        public AddTransactionViewModel()
+        {
+            AvailableAssets = new ObservableCollection<Coin>(Coin.GetAvailableAssets());
+        }
 
+        private bool _isDeposit;
         public bool IsDeposit
         {
             get => _isDeposit;
             set { SetProperty(ref _isDeposit, value); }
-         }
+        }
 
         private string _id;
         public string Id
@@ -24,6 +32,34 @@ namespace Cryptollet.Modules.AddTransaction
             {
                 _id = Uri.UnescapeDataString(value);
             }
+        }
+
+        private ObservableCollection<Coin> _availableAssets;
+        public ObservableCollection<Coin> AvailableAssets
+        {
+            get => _availableAssets;
+            set { SetProperty(ref _availableAssets, value); }
+        }
+
+        private Coin _selectedCoin;
+        public Coin SelectedCoin
+        {
+            get => _selectedCoin;
+            set { SetProperty(ref _selectedCoin, value); }
+        }
+
+        private DateTime _transactionDate;
+        public DateTime TransactionDate
+        {
+            get => _transactionDate;
+            set { SetProperty(ref _transactionDate, value); }
+        }
+
+        public ICommand AddTransactionCommand { get => new Command(async () => await AddTransaction()); }
+
+        private async Task AddTransaction()
+        {
+
         }
     }
 }
