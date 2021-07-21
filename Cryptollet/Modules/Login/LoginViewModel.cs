f1ab1d6fc36b24b9fe4a09cd5ee4bedf2b20568c
Stuart Bowes <stuartbowes2000@gmail.com>
2021-07-21 13:34:24 +0100

    Navigate to the RegisterView in LoginView

--- Cryptollet/Modules/Login/LoginViewModel.cs
+++ Cryptollet/Modules/Login/LoginViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using Cryptollet.Common.Base;
+using Cryptollet.Common.Navigation;
+using Cryptollet.Modules.Register;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Cryptollet.Modules.Login
 {
     public class LoginViewModel : BaseViewModel
     {
+        private INavigationService _navigationService;
+
+        public LoginViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ICommand RegisterCommand { get => new Command(async () => await GoToRegister()); }
+
+        private async Task GoToRegister()
+        {
+            await _navigationService.InsertAsRoot<RegisterViewModel>();
+        }
     }
 }
